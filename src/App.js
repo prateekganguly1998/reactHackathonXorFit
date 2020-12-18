9ab41b989ffe47f6d7598ef728f1fd9d3c09da42
Prateek Gangopadhyay <gangopadhyay_p@India.XoriantCorp.com>
2020-12-18 12:28:46 +0530

    practice

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import XorFit from './XorFit';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <XorFit/>
+      </div>
+    );
+  }
+}
+
+export default App;
